8f5f7b39bccc6c18db687c006abcbf232c3aee6b
luxsoo <luxsoo@naver.com>
2026-09-14 12:42:16 +0900

    fix: LoginRequestDto 수정

--- src/main/java/com/piniters/pinit/dto/LoginRequestDto.java
+++ src/main/java/com/piniters/pinit/dto/LoginRequestDto.java
@@ -2,7 +2,7 @@ package com.piniters.pinit.dto;
 
 @lombok.Getter
 @lombok.Setter
-public static class LoginRequestDto {
+public class LoginRequestDto {
     private String provider;    // "KAKAO" 또는 "GOOGLE"
     private String accessToken; // 프론트엔드가 소셜 로그인 후 받아온 토큰
 }
